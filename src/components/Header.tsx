666b3ad06903b9e4eea67ecedc3e10f5b7e08beb
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-07-14 02:02:11 +0300

    Modified the Nav Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import ThemeToggle from './theme-toggle'
 import { RainbowButton } from './magicui/rainbow-button'
+import { AuroraText } from "@/components/magicui/aurora-text";
 
 export default function HeaderSection() {
   return (
     <nav className=" flex justify-between items-center bg-background px-4 py-2">
       <div className=" flex justify-center items-center">
-        <h1 className=' font-bold'>Dust Aggregator</h1>
+        <AuroraText className=' font-bold text-2xl'>Dust Aggregator</AuroraText>
       </div>
       <div className=" flex justify-center items-center">
         <ThemeToggle />
