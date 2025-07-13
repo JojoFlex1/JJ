598287460dfef1e363a4a566e4b61b6c99367a88
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-07-13 09:46:43 +0300

    Configured Themes

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function HeaderSection() {
+  return (
+      <nav>
+          <div>
+              <h1>Dust Aggregator</h1>
+              
+          </div>
+    </nav>
+  )
+}
