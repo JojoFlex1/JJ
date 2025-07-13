7d0cb803a488bfa999c540149d422333f3d661ff
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-07-13 16:33:59 +0300

    Configured the NavBar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import { RainbowButton } from './magicui/rainbow-button'
+import ThemeToggle from './theme-toggle'
+import { Wallet } from 'lucide-react'
 
 export default function HeaderSection() {
   return (
-      <nav>
-          <div>
-              <h1>Dust Aggregator</h1>
-              
-          </div>
+    <nav className=" flex justify-between items-center mx-4">
+      <div className=" flex justify-center items-center">
+        <h1>Dust Aggregator</h1>
+      </div>
+      <div className=" flex justify-center items-center">
+        <ThemeToggle />
+        <RainbowButton variant="outline">
+          <Wallet />
+          Connect Wallet
+        </RainbowButton>
+      </div>
     </nav>
   )
 }
---
+++ tailwind.config.ts
@@ -0,0 +1,22 @@
+import type { Config } from "tailwindcss";
+import animatePlugin from "tailwindcss-animate";
+
+const config: Config = {
+  darkMode: "class",
+  content: [
+    "./src/app/**/*.{ts,tsx}",
+    "./src/components/**/*.{ts,tsx}",
+    "./src/pages/**/*.{ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [animatePlugin],
+};
+
+export default config;
