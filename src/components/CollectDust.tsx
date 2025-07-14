30733be190dea982c2c72407ac2be2cf45ce93be
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-07-14 10:09:40 +0300

    Configured the Collect Dust Wallets

--- src/components/CollectDust.tsx
+++ src/components/CollectDust.tsx
@@ -1,9 +1,15 @@
 'use client';
+import WalletCardComponent from "./WalletCard";
 
 export default function CollectDustComponent() {
   return (
       <div>
-          <h1 className=" font-bold text-lg mt-2">Connect Your Wallets</h1>
-    </div>
+          <WalletCardComponent wallet={'Ethereum Wallet'} description="0x1a2...3b4c" />
+          <WalletCardComponent wallet={'Polygon Wallet' } description="0x5d6...7e8f"/>
+          <WalletCardComponent wallet={'Avalanche Wallet'} description="0x9g0...1h2i"/>
+          <WalletCardComponent wallet={'Binance Smart Chain'} description="0x3j4...5k6l"/>
+          <WalletCardComponent wallet={'Solana Wallet'} description="0b7q6...1d7z" />
+      </div>
   )
 }
+
--- src/components/WalletCard.tsx
+++ src/components/WalletCard.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+'use client'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardTitle,
+} from '@/components/ui/card'
 
-export default function WalletCardComponent() {
+import { BorderBeam } from '@/components/magicui/border-beam'
+
+export default function WalletCardComponent({ wallet, description }: {
+    wallet: string,
+    description: string
+}) {
   return (
-      <div>
-          
-    </div>
+    <Card className="relative overflow-hidden p-2 mb-2">
+      <CardContent className=' flex items-center justify-between '>
+        <div>
+            <CardTitle>{wallet}</CardTitle>
+            <CardDescription>
+              {description}
+            </CardDescription>
+        </div>
+        <div>
+            <Button
+              className="relative overflow-hidden"
+              size="lg"
+              variant="outline"
+            >
+              Connect
+              <BorderBeam
+                size={40}
+                initialOffset={20}
+                className="from-transparent via-yellow-500 to-transparent"
+                transition={{
+                  type: 'spring',
+                  stiffness: 60,
+                  damping: 20,
+                }}
+              />
+            </Button>
+        </div>
+      </CardContent>
+
+      <BorderBeam duration={8} size={100} />
+    </Card>
   )
 }
