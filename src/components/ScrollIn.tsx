e383fa0636efd8d9c95010203e91da4a9074aab4
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-07-15 19:24:17 +0300

    Configured the Dust balances section

--- src/components/ScrollIn.tsx
+++ src/components/ScrollIn.tsx
@@ -1,24 +1,50 @@
-'use client';
-import React from "react";
+'use client'
+import React from 'react'
 
-import { ScrollArea } from "@/components/ui/scroll-area"
-import { Separator } from "@/components/ui/separator"
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { Separator } from '@/components/ui/separator'
+import {
+  Card,
+  CardAction,
+  CardDescription,
+  CardTitle,
+  CardHeader,
+} from '@/components/ui/card'
+import { Checkbox } from '@/components/ui/checkbox'
 
-const tags = Array.from({ length: 50 }).map(
-  (_, i, a) => `v1.2.0-beta.${a.length - i}`
-)
+const CardSection = ({
+  token,
+  tokenShort,
+  price,
+}: {
+  token: string
+  tokenShort: string
+  price: number
+}) => {
+  return (
+    <Card className=" p-2">
+      <CardHeader>
+        <CardTitle>
+          {price} {tokenShort}
+        </CardTitle>
+        <CardAction className=" flex items-center gap-2 mt-2">
+          ${price} <Checkbox className=" !bg-accent" />
+        </CardAction>
+        <CardDescription>{token}</CardDescription>
+      </CardHeader>
+    </Card>
+  )
+}
 
 export default function ScrollComponent() {
   return (
     <ScrollArea className="h-72 rounded-md border w-full">
       <div className="p-4">
-        <h4 className="mb-4 text-sm leading-none font-medium">Tags</h4>
-        {tags.map((tag) => (
-          <React.Fragment key={tag}>
-            <div className="text-sm">{tag}</div>
-            <Separator className="my-2" />
-          </React.Fragment>
-        ))}
+        <CardSection token="Ethereum" tokenShort="ETH" price={0.42} />
+        <Separator className=" my-2" />
+        <CardSection token="Stellar" tokenShort="XLM" price={0.54} />
+        <Separator className=" my-2" />
+        <CardSection token="Starknet" tokenShort="STRK" price={0.13} />
       </div>
     </ScrollArea>
   )
