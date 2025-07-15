b1925e6dfaa16087e1118308132e21df0f533115
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-07-15 14:13:27 +0300

    Added the Info section at the Title

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,11 +1,31 @@
 import TabsInSection from './TabsIn'
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/hover-card"
+import { Info } from 'lucide-react'
+
+const InfoSection = () => {
+    return (
+        <HoverCard>
+            <HoverCardTrigger>
+                <Info/>
+  </HoverCardTrigger>
+  <HoverCardContent>
+                <h1 className=' font-bold text-lg underline'>About Dust Aggregation</h1>
+                <p>Dust refers to tiny amounts of cryptocurrency that are too small to be transacted due to network fees exceeding their value. This tool helps you reclaim value from these otherwise unusable assets by batching them together and moving them to Stellar&apos;s low-fee environment.</p>
+  </HoverCardContent>
+</HoverCard>
+    )
+}
 
 export default function HomeSection() {
   return (
     <div className=" min-h-screen mx-6 my-4 rounded-3xl bg-accent">
       <div className=" p-4">
-        <h1 className=" font-bold text-2xl mb-4">
-          Cross-Chain Dust Aggregator
+        <h1 className=" font-bold text-2xl mb-4 flex items-center gap-4">
+          Cross-Chain Dust Aggregator <InfoSection/>
         </h1>
         <p className=" text-foreground">
           Collect small, unusable balances from different wallets, batch process
