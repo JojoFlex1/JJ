af3805d8e14c59a8e66b4d02a1f71b157b2e6377
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-07-16 13:10:12 +0300

    Migrated the Root Wallet button to an independent component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,17 @@
 import React from 'react'
 import ThemeToggle from './theme-toggle'
-import { RainbowButton } from './magicui/rainbow-button'
-import { AuroraText } from "@/components/magicui/aurora-text";
+import RootWalletButton from './RootWallet'
+import { AuroraText } from '@/components/magicui/aurora-text'
 
 export default function HeaderSection() {
   return (
     <nav className=" flex justify-between items-center bg-background px-4 py-2">
       <div className=" flex justify-center items-center">
-        <AuroraText className=' font-bold text-2xl'>Dust Aggregator</AuroraText>
+        <AuroraText className=" font-bold text-2xl">Dust Aggregator</AuroraText>
       </div>
       <div className=" flex justify-center items-center">
         <ThemeToggle />
-              <RainbowButton variant={'outline'} className=' bg-accent'>
-                  Connect Wallet
-        </RainbowButton>
+        <RootWalletButton />
       </div>
     </nav>
   )
---
+++ src/components/RootWallet.tsx
@@ -0,0 +1,9 @@
+import { RainbowButton } from './magicui/rainbow-button'
+
+export default function RootWalletButton() {
+  return (
+    <RainbowButton variant={'outline'} className=' bg-accent'>
+                      Connect Wallet
+            </RainbowButton>
+  )
+}
