f1cd79e05996b9cd33af7b70e98f02bc328b1159
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-07-21 20:57:26 +0300

    Removed the Process & Transfer Tab section coz it's no longer needed

--- src/components/TabsIn.tsx
+++ src/components/TabsIn.tsx
@@ -1,19 +1,21 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
-import CollectDustComponent from "./CollectDust"
-import ProcessAndTransferComponent from "./Process&Transfer"
-import SwapOnStellarComponent from "./SwapOnStellar"
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
+import CollectDustComponent from './CollectDust'
+import SwapOnStellarComponent from './SwapOnStellar'
 
 export default function TabsInSection() {
   return (
     <Tabs defaultValue="dust" className=" w-full">
-  <TabsList className=" w-full">
-    <TabsTrigger value="dust">Collect Dust</TabsTrigger>
-    <TabsTrigger value="process">Process & Transfer</TabsTrigger>
-    <TabsTrigger value="swap">Swap on Stellar</TabsTrigger>
-  </TabsList>
-  <TabsContent value="dust"><CollectDustComponent/></TabsContent>
-  <TabsContent value="process"><ProcessAndTransferComponent/></TabsContent>
-  <TabsContent value="swap"><SwapOnStellarComponent/></TabsContent>
-</Tabs>
+      <TabsList className=" w-full">
+        <TabsTrigger value="dust">Collect Dust</TabsTrigger>
+        <TabsTrigger value="swap">Swap on Stellar</TabsTrigger>
+      </TabsList>
+      <TabsContent value="dust">
+        <CollectDustComponent />
+      </TabsContent>
+
+      <TabsContent value="swap">
+        <SwapOnStellarComponent />
+      </TabsContent>
+    </Tabs>
   )
 }
